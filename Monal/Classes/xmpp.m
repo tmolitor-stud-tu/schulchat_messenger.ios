21be60b1e4f3903548daff1fd48e25435814d353
Thilo Molitor <thilo@eightysoft.de>
2021-01-30 10:43:15 +0100

    KWO: implement kwo auth scheme

--- Monal/Classes/xmpp.m
+++ Monal/Classes/xmpp.m
@@ -5,6 +5,8 @@
 //  Created by Anurodh Pokharel on 6/29/13.
 //
 //
+//KWO app secrets no published at github
+#include "../local/secrets.h"
 
 #import <CommonCrypto/CommonCrypto.h>
 #import <CFNetwork/CFSocketStream.h>
@@ -1685,11 +1687,30 @@ NSString *const kData=@"data";
                 return [self invalidXMLError];
             if(self.connectionProperties.server.isDirectTLS || self->_startTLSComplete)
             {
-                MLXMLNode* responseXML = [[MLXMLNode alloc] initWithElement:@"response" andNamespace:@"urn:ietf:params:xml:ns:xmpp-sasl"];
-
-                //TODO: implement SCRAM SHA1 and SHA256 based auth
-
-                [self send:responseXML];
+                NSString* challenge = [[NSString alloc] initWithData:[parsedStanza findFirst:@"/#|base64"] encoding:NSUTF8StringEncoding];
+                DDLogVerbose(@"kwo challenge: %@", challenge);
+                
+                //gather and build plaintext data string
+                NSString* deviceID = [[[UIDevice currentDevice] identifierForVendor] UUIDString];
+                //NSString* deviceName = [NSString stringWithFormat:@"%@ (%@)", [[UIDevice currentDevice] name], [[UIDevice currentDevice] localizedModel]];
+                NSString* deviceName = [[UIDevice currentDevice] name];
+                NSString* versionCode = [[[NSBundle mainBundle] infoDictionary] objectForKey:(NSString*)kCFBundleVersionKey];
+                NSString* plaintextDeviceDescription = [NSString stringWithFormat:@"%@|%@|%@|%@", deviceID, deviceName, versionCode, @"ios"];
+                
+                //calculate hmac signatures
+                NSString* firstHMAC = [HelperTools stringSha256HmacForKey:self.connectionProperties.identity.password andData:plaintextDeviceDescription];
+                NSString* secondHMAC = [HelperTools stringSha256HmacForKey:APP_SECRET andData:firstHMAC];
+                NSString* finalHMAC = [HelperTools stringSha256HmacForKey:challenge andData:secondHMAC];
+                DDLogVerbose(@"kwo hmac result: %@", finalHMAC);
+                
+                //send response containing plaintext data and signature
+                [self send:[[MLXMLNode alloc]
+                    initWithElement:@"response"
+                    andNamespace:@"urn:ietf:params:xml:ns:xmpp-sasl"
+                    withAttributes:@{}
+                    andChildren:@[]
+                    andData:[HelperTools encodeBase64WithString:[NSString stringWithFormat:@"%@|%@", plaintextDeviceDescription, finalHMAC]]
+                ]];
                 return;
             }
         }
@@ -1743,7 +1764,18 @@ NSString *const kData=@"data";
                     //extract menchanisms presented
                     NSSet* supportedSaslMechanisms = [NSSet setWithArray:[parsedStanza find:@"{urn:ietf:params:xml:ns:xmpp-sasl}mechanisms/mechanism#"]];
                     
-                    if([supportedSaslMechanisms containsObject:@"PLAIN"])
+                    //KWO sasl auth
+                    if([supportedSaslMechanisms containsObject:@"KWO"])
+                    {
+                        [self send:[[MLXMLNode alloc]
+                            initWithElement:@"auth"
+                            andNamespace:@"urn:ietf:params:xml:ns:xmpp-sasl"
+                            withAttributes:@{@"mechanism": @"KWO"}
+                            andChildren:@[]
+                            andData:[HelperTools encodeBase64WithString:self.connectionProperties.identity.user]
+                        ]];
+                    }
+                    else if([supportedSaslMechanisms containsObject:@"PLAIN"])
                     {
                         [self send:[[MLXMLNode alloc]
                             initWithElement:@"auth"
@@ -2277,7 +2309,9 @@ NSString *const kData=@"data";
 {
     _accountState = kStateBinding;
     XMPPIQ* iqNode =[[XMPPIQ alloc] initWithType:kiqSetType];
-    [iqNode setBindWithResource:resource];
+    //[iqNode setBindWithResource:resource];
+    //KWO: use vendor uuid as resource as required by kwo auth scheme
+    [iqNode setBindWithResource:[[[UIDevice currentDevice] identifierForVendor] UUIDString]];
     [self sendIq:iqNode withHandler:$newHandler(MLIQProcessor, handleBind)];
 }
 
